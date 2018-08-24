c79e810cb3ba8b08c29eddb0607a00f370f5bfaa
Vijay Reddy <vijaymail1776@gmail.com>
2018-08-24 10:40:45 -0500

    AppDatabase , MyApplication

--- app/src/main/java/project/docs/files/template_kotlin/data/AppDatabase.kt
+++ app/src/main/java/project/docs/files/template_kotlin/data/AppDatabase.kt
@@ -1,13 +1,35 @@
 package project.docs.files.template_kotlin.data
 
-import android.arch.persistence.db.SupportSQLiteOpenHelper
-import android.arch.persistence.room.DatabaseConfiguration
-import android.arch.persistence.room.InvalidationTracker
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
+import android.content.Context
 
 
+
+
+private val LOG_TAG = AppDatabase::class.java!!.getSimpleName()
+private val DATABASE_NAME = "item_database"
+
+
+
+
+@Database(entities = [(Item::class)], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
- 
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+        fun getDataBase(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, DATABASE_NAME).build()
+            }
+            return INSTANCE as AppDatabase
+        }
+    }
+
+
+    abstract fun ItemDao(): ItemDao
+
 
 }
