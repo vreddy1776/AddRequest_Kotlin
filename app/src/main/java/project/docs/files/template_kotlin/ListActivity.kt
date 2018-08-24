9b73242091ef319f7b61a469054d541211c2cb85
Vijay Reddy <vijaymail1776@gmail.com>
2018-08-23 20:45:09 -0500

    added ListActivityv

---
+++ app/src/main/java/project/docs/files/template_kotlin/ListActivity.kt
@@ -0,0 +1,14 @@
+package project.docs.files.template_kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+
+}
--- app/src/main/java/project/docs/files/template_kotlin/MainActivity.kt
+++ app/src/main/java/project/docs/files/template_kotlin/MainActivity.kt
@@ -1,12 +1,50 @@
 package project.docs.files.template_kotlin
 
+import project.docs.files.template_kotlin.ListActivity
+import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.v4.content.ContextCompat
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBar()
+        goToListActivity()
     }
+
+
+    private fun setupActionBar() {
+
+        val window = this.window
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimary)
+        }
+
+    }
+
+
+    private fun goToListActivity() {
+
+        val intent = Intent(this, ListActivity::class.java)
+
+        val handler = Handler()
+        handler.postDelayed({ startActivity(intent) }, 3000)   //3 second
+    }
+
+
+    override fun onBackPressed() {
+        // do nothing
+    }
+
+
 }
